cc274b50cf6b2cbf131691b40acb3b47e79bdf85
Chanchana Wicha <chanchana@wongnai.com>
2020-07-17 21:54:45 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import {Grid, Container, Button} from '@material-ui/core'
 import './App.css';
 
+const NavBar = () => (
+  <AppBar position="static">
+    <Toolbar>
+        <Typography variant="title" color="inherit">
+          Rand Math!
+        </Typography>
+    </Toolbar>
+  </AppBar>
+)
+
 function App() {
+  const numbersLength = 5
+  const [numbers, setNumbers] = useState([0, 0, 0, 0, 0])
+  const [restNumber, setResultNumber] = useState(0)
+
+  const actionRandom = () => {
+    let newNumbers = []
+    for (let i = 0; i < numbersLength; i++) {
+      newNumbers.push(Math.floor(Math.random() * 10))
+    }
+    let newResultNumber = Math.floor(Math.random() * 100)
+    setNumbers(newNumbers)
+    setResultNumber(newResultNumber)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {NavBar()}
+        <Container maxWidth="sm">
+          <Grid container direction="row" justify="space-around" alignItems="center">
+            {numbers.map(number => (
+              <Grid container item xs={1}>
+                <h2>{number}</h2>
+              </Grid>
+            ))}
+          </Grid>
+          <Grid container direction="column" justify="center" alignItems="center">
+            <h1>{restNumber}</h1>
+            <Button variant="contained" color="primary" onClick={actionRandom}>New</Button>
+          </Grid>
+
+
+        </Container>
+    </>
   );
 }
 
